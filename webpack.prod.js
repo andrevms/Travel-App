d18e6f19ff5a6d469e974b7cc78ab3cd1f2a1f22
andrevms <andrevms@hotmail.com>
2020-07-29 10:39:56 -0300

    Fix: webpack and npm modules install and up to use

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,49 @@
-const path = require('path');
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
 
 module.exports = {
-    mode: "production",
-    entry: "./src/client/index.js",
+    entry: './src/client/index.js',
+    mode: 'production',
+    optimization: {
+        minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
+    },
+    output: {
+        libraryTarget: 'var',
+        library: 'Client'
+    },
+    module: {
+        rules: [
+            {
+                test: '/\.js$/',
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
+            },
+            {
+                parser: {
+                  amd: false
+                }
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: ['file-loader',],
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html",
+        }),
+        new MiniCssExtractPlugin({ filename: "[name].css" })
+    ]
 }
