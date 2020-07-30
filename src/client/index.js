0d2ab3d8a95c8f305190fa3f014203ece4f7f1de
andrevms <andrevms@hotmail.com>
2020-07-29 21:00:46 -0300

    feat: ProtoType Layout

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,6 @@
+//css
 import './styles/header.scss'
-
-
+import './styles/base.scss'
 //Images
 import header from './images/header.jpg';
+//js
---
+++ src/server/index.js
@@ -0,0 +1,41 @@
+// Setup empty JS object to act as endpoint for all routes
+let projectData = [];
+
+// Express to run server and routes
+const express = require('express')
+
+// Start up an instance of app
+const app = express();
+
+/* Dependencies */
+var path = require('path')
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const { url } = require('inspector');
+
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+app.use(cors());
+
+// Initialize the main project folder
+app.use(express.static('dist'))
+
+// Setup Server
+app.listen(3000, () => {
+    console.log(`Server up => Try 3000`)
+})
+
+// :::::: Routes ::::::
+
+// Init Route
+app.get('/', function (req, res) {
+    res.sendFile('dist/index.html')
+})
+
+app.post('/form', function (req, res) {
+    res.send('Got a POST request')
+})
