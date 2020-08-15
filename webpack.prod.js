a34e39fc1c4f68233bd3f19c038384a29955db2e
andrevms <andrevms@hotmail.com>
2020-08-14 22:03:35 -0300

    chore: add Clean webpack plugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,6 +5,7 @@ const HtmlWebPackPlugin = require("html-webpack-plugin")
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const WorkboxPlugin = require('workbox-webpack-plugin');
 
@@ -49,6 +50,8 @@ module.exports = {
 
         new MiniCssExtractPlugin({ filename: "[name].css" }),
 
-        new WorkboxPlugin.GenerateSW()
+        new WorkboxPlugin.GenerateSW(),
+        
+        new CleanWebpackPlugin({dry: false}),
     ],
 }
